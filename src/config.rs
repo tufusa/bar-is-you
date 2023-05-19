6cccff721cc604219a3afeac62622be14d8b4954
Tufusa <louptung@gmail.com>
2023-05-19 10:00:50 +0900

    feat: config struct

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,20 @@
-pub const SCREEN_WIDTH: u32 = 860;
-pub const SCREEN_HEIGHT: u32 = 540;
+use bevy::prelude::*;
 
-pub const BLOCK_WIDTH: u32 = 60;
-pub const BLOCK_HEIGHT: u32 = 30;
+pub struct Screen;
+pub struct Block;
+pub struct Ball;
+
+impl Screen {
+    pub const SIZE: Vec2 = Vec2 { x: 860., y: 540. };
+}
+
+impl Block {
+    pub const SIZE: Vec2 = Vec2 { x: 60., y: 30. };
+    pub const GAP: f32 = 5.;
+    pub const COLOR: Color = Color::hex("FFFFFF").unwrap();
+}
+
+impl Ball {
+    pub const RADIUS: f32 = 30.;
+    pub const COLOR: Color = Color::hex("FFFFFF").unwrap();
+}
